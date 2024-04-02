e93fb88b9e4c602336e0b2839b4820adf92d555a
andrewtroicki <atroicki@icloud.com>
2024-04-02 11:44:53 +1100

    tests complete

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ let num1 = 31;
 let num2 = 2;
 let multiply = num1 * num2;
 console.log(multiply);
+
+let random = Math.ceil(Math.random() *10);
+console.log(random);
+
+let num3 = 16;
+let num4 = 4;
+
+let mod = (16 / 4)
+console.log(mod);
+
+
+let max = Math.max (1, 2, 3, 4, 5, 10, 15, 20);
+console.log(max);
+
